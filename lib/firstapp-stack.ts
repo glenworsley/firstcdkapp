830d598307c171eaf65ec6976ed8921c0eb7aeb4
Glen Worsley <glen.worsley@shinesolutions.com>
2020-03-26 08:22:30 +1100

    more complex stack

--- lib/firstapp-stack.ts
+++ lib/firstapp-stack.ts
@@ -1,15 +1,43 @@
-import * as cdk from '@aws-cdk/core';
-import * as s3 from '@aws-cdk/aws-s3';
+import autoscaling = require('@aws-cdk/aws-autoscaling');
+import ec2 = require('@aws-cdk/aws-ec2');
+import elbv2 = require('@aws-cdk/aws-elasticloadbalancingv2');
+import cdk = require('@aws-cdk/core');
+import { UserData } from '@aws-cdk/aws-ec2';
 
 export class FirstappStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, 'MyFirstBucket', {
-      versioned: true,
-      encryption: s3.BucketEncryption.KMS_MANAGED
+    // The code that defines your stack goes here
+    // This stack is made up of a VPC, ASG, ELB.
+    // The ASG will use t2.micro Instances, created from the AmazonLinuxImage.
+    const vpc = new ec2.Vpc(this, 'VPC');
+
+    const asg = new autoscaling.AutoScalingGroup(this, 'ASG', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      machineImage: new ec2.AmazonLinuxImage(),
     });
 
-    // The code that defines your stack goes here
+    const lb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
+      vpc,
+      internetFacing: true
+    });
+
+    const listener = lb.addListener('Listener', {
+      port: 80,
+    });
+
+    listener.addTargets('Target', {
+      port: 80,
+      targets: [asg]
+    });
+
+    listener.connections.allowDefaultPortFromAnyIpv4('Open to the world');
+
+    asg.scaleOnRequestCount('AModestLoad', {
+      targetRequestsPerSecond: 1
+    });
+
   }
 }
